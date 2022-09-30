ad00c86fdfd4ab68013a14334de683fe41bf7457
Jacob H <jacob.hannah164@gmail.com>
2022-09-29 20:07:14 -0400

    created models and altered server to be an apollo server to handle graphql/created seeds file to populate db with emails and passwords

--- server/server.js
+++ server/server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
+const {typeDefs, resolvers} = require('./schema');
+const {authMiddleware} = require('./utils/auth');
 
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({typeDefs, resolvers, context: authMiddleware})
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
@@ -13,8 +16,24 @@ app.use(express.json());
 // serve static assets
 // app.use('/cover'), express.static(path.join(__dirname, '../client/cover'));
 
-db.once('open', () => {
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({app});
+
+  if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  })
+
+  db.once('open', () => {
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
-    })
-})
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`)
+    });
+  });
+}
+
+startApolloServer(typeDefs, resolvers);
