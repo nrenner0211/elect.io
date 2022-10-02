3ee32080e13dcd6f5eac0cbf2238d354c3ed5255
Davon <dkwebb95@gmail.com>
2022-10-02 00:59:49 -0400

    resturctuing homepage

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -6,8 +6,7 @@ import Contact from '../Contact';
 function Footer () {
     return(
         <section className='my-5'>
-            <h1 id='footer' className='intro'>Footer</h1>
-            
+            <h1 id='footer' className='intro'>What to do</h1>          
             <div className='card'>
                 <Contact></Contact>
             </div>
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,17 @@
 import React from "react";
 
 const Home = () => {
-    return (
-    <div className="intro">Home</div>
-
-    )
-}
+  return (
+    <section>
+      <div className="border-bg">
+        <h1 className="intro">Our Mission</h1>
+        <h2 className="body-text">
+          Teenagers and young adults are constantly getting involved into
+          politics and more than ever before wants to get their voices heard.
+        </h2>
+      </div>
+    </section>
+  );
+};
 
 export default Home;
