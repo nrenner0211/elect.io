4dc3cfdbb291a5b132aa42c7eb41842764b4c97c
Jacob H <jacob.hannah164@gmail.com>
2022-10-06 14:17:23 -0400

    created setWatchList to update the page based on address entered by user

--- client/src/components/RepList/index.js
+++ client/src/components/RepList/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import civicInfo from '../../api/civicInfo';
+import TextField from '@mui/material/TextField';
 
 // mock data
 // const mockData = require('../../api/data.json');
@@ -8,7 +9,7 @@ import civicInfo from '../../api/civicInfo';
 
 export const RepList = () => {
     const [representatives, setRepresentatives] = useState([])
-    const [watchList] = useState(['131 Riverview AVE, Wheeling WV 26003'])
+    const [watchList, setWatchList] = useState(['131 Riverview AVE, Wheeling WV 26003'])
 
     // MOCKDATA
     // let representatives = mockData.officials;
@@ -30,14 +31,13 @@ export const RepList = () => {
                     })
                 }))
                 
-                //console.log(responses)
+                // console.log(responses)
                 const repData = responses.map((response) => {
                 return  {
-                        data: response.data.offices,
                         officials: response.data.officials
                     }
                 })
-                //console.log(repData[0])
+                console.log('repData', repData)
                 if (isMounted) {
                     setRepresentatives(repData[0].officials)
                 }
@@ -52,33 +52,54 @@ export const RepList = () => {
         return () => (isMounted = false)
     }, [watchList]) 
 
+// testing data chain
+console.log('representatives', representatives)
+
+
 return (
-    <div>
-        <table className='table'>
-            <thead style={{ color: '#004FFF'}}>
-                <tr>
-                    <th scope='col'>Name</th>
-                    <th scope='col'>Party</th>
-                    <th scope='col'>Phone</th>
-                    <th scope='col'>Website</th>
-                    
-                </tr>
-            </thead>
-            <tbody>
-                {representatives.map((item) => {
-                    return ( 
-                        <>                  
-                        <tr>
-                            <th scope='row'>{item.name}</th>
-                            <td>{item.party}</td>
-                            <td>{item.phones}</td>
-                            <td>{item.urls}</td> 
-                        </tr>
-                        </>
-                    )
-                })}
-            </tbody>
-        </table>
-    </div>
-    )
+    <>
+        <h4>Enter Address:</h4>
+    
+        {/* textfield component is loaded with label and such */}
+    
+        <form className="addressForm center">
+            <TextField className='userInput' id="outlined-basic" label="Address" variant="outlined"/>
+            <button className="btn" type="submit" onClick={(event) => {
+                event.preventDefault();
+                console.log(document.getElementById('outlined-basic').value);
+                setWatchList(document.getElementById('outlined-basic').value)
+            }}>Submit</button>
+        </form>
+    
+        <h5>Powered by the Civic Information API, <a href='https://developers.google.com/civic-information'>Learn More</a></h5>
+    
+        <div>
+            <table className='table'>
+                <thead style={{ color: '#004FFF'}}>
+                    <tr>
+                        <th scope='col'>Name</th>
+                        <th scope='col'>Party</th>
+                        <th scope='col'>Phone</th>
+                        <th scope='col'>Website</th>
+                        
+                    </tr>
+                </thead>
+                <tbody>
+                    {representatives.map((item) => {
+                        return ( 
+                            <>                  
+                            <tr>
+                                <th scope='row'>{item.name}</th>
+                                <td>{item.party}</td>
+                                <td>{item.phones}</td>
+                                <td>{item.urls}</td> 
+                            </tr>
+                            </>
+                        )
+                    })}
+                </tbody>
+            </table>
+        </div>
+    </>
+        )
 }
